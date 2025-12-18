c4861d613ec13c3153f174280b56d0f0001d1b4d
RSamaium <sam@newbot.io>
2025-12-18 14:16:04 +0100

    user dialog

---
+++ src/app/user-edit/user-dialog.ts
@@ -0,0 +1,32 @@
+import { DialogRef } from "@angular/cdk/dialog";
+import { Component, inject } from "@angular/core";
+
+export type DialogInput = {
+    title: string
+}
+
+@Component({
+    selector: 'app-user-dialog',
+    template: `
+        <div class="dialog">
+            <h1>{{ data.title }}</h1>
+            <button (click)="close()">Fermer</button>
+        </div>
+    `,
+    styles: `
+        .dialog {
+            backgroundColor: white;
+        }
+    `
+})
+export class UserDialog {
+    private dialogRef = inject(DialogRef<void, DialogInput>)
+
+    get data(): DialogInput {
+        return this.dialogRef.config.data
+    }
+
+    close() {
+        this.dialogRef.close()
+    }
+}
--- src/app/user-edit/user-edit.ts
+++ src/app/user-edit/user-edit.ts
@@ -5,6 +5,8 @@ import { rxResource, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { switchMap, tap } from 'rxjs';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { CanComponentDeactivate } from '../core/guards/confirm.guard';
+import { Dialog } from '@angular/cdk/dialog';
+import { DialogInput, UserDialog } from './user-dialog';
 
 @Component({
   selector: 'app-user-edit',
@@ -29,6 +31,7 @@ export class UserEdit implements CanComponentDeactivate {
 
  private userService = inject(UserService)
  private builder = inject(FormBuilder)
+ private dialog = inject(Dialog)
 
  form = this.builder.group({
   name: ''
@@ -50,5 +53,19 @@ export class UserEdit implements CanComponentDeactivate {
 
   modified(): boolean {
     return this.form.dirty
+  }
+
+  openRoleDialog() {
+    const dialogRef = this.dialog.open<void, DialogInput>(UserDialog, {
+      height: '400px',
+      width: '600px',
+      data: {
+        title: 'Mon Titre'
+      }
+    })
+
+    dialogRef.closed.subscribe(() => {
+      console.log('fermé')
+    })
   }
 }

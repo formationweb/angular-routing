4cbfd85f00b40466123c1b4516a2b575ea8f600c
RSamaium <sam@newbot.io>
2025-12-18 16:51:50 +0100

    home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  //isConnected = signal(false)
+  isConnected = new BehaviorSubject(false)
 
+  constructor() {
+    setTimeout(() => {
+      this.isConnected.next(true)
+    }, 2000)
+  }
 }

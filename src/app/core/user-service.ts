9d9ad1a88efbd6f549ab43ec934a4cd6065a061b
RSamaium <sam@newbot.io>
2025-12-18 11:27:02 +0100

    permissions user

--- src/app/core/user-service.ts
+++ src/app/core/user-service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 export interface User {
   id: number
   name: string
+  permissions: string[] // ['user.read', 'user.edit']
 }
 
 @Injectable({
@@ -16,6 +17,13 @@ export class UserService {
   readonly url = 'https://jsonplaceholder.typicode.com/users'
 
   getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.url + '/' + id)
+    return this.http.get<User>(this.url + '/' + id).pipe(
+      map((user) => {
+        return {
+          ...user,
+          permissions: ['user.read', 'user.edit']
+        }
+      })
+    )
   }
 }
